549fd801efe77ce85a7bab1b35bfaa5f3d666e4a
wpjscc <wpjscc@gmail.com>
2023-10-08 15:40:57 +0800

    打孔

--- src/Tunnel/Client/Tunnel/P2pTunnel.php
+++ src/Tunnel/Client/Tunnel/P2pTunnel.php
@@ -315,6 +315,8 @@ class P2pTunnel extends EventEmitter implements ConnectorInterface, \Wpjscc\Pene
                             continue;
                         }
 
+                        $this->server->send("HTTP/1.1 414 punch \r\n" . $this->header, $peer);
+
                         // 没被连上才可以连, 一个 tcp client 只能连一个 对端,(大概率是nat对tcp限制了)
                         if (!PeerManager::localAddressIsPeerd('tcp://'. $this->localAddress) && !in_array('tcp://'.$peer,PeerManager::getTcpPeeredAddrs())) {
                             static::getLogger()->debug("P2pTunnel::" . __FUNCTION__ . " tcp peering", [
