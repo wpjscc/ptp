071ddf2f9f141389196e862a628e54f790cb7d6a
wpjscc <wpjscc@gmail.com>
2023-10-17 22:55:20 +0800

    watch

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,27 @@
+// var browserify = require('browserify');
+// var watchify = require('watchify');
+// var b = browserify({ cache: {}, packageCache: {} });
+// b.plugin(watchify);
+// b.add('./src/client.js');
+// b.bundle().pipe(process.stdout);
+
+var fs = require('fs');
 var browserify = require('browserify');
-var b = browserify();
-b.add('./src/client.js');
-b.bundle().pipe(process.stdout);
+var watchify = require('watchify');
+
+var b = browserify({
+  entries: ['./src/client.js'],
+  cache: {},
+  packageCache: {},
+  plugin: [watchify]
+});
+
+b.on('update', bundle);
+bundle();
+
+function bundle() {
+  b.bundle()
+    .on('error', console.error)
+    .pipe(fs.createWriteStream('build/client.js'))
+  ;
+}
